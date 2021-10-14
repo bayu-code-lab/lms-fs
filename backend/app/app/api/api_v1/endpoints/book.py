9cb36c63526b109cba9188043c7d79f1c2d5d72d
Bayu Satria Gemilang <bayusatria@Bayus-MacBook-Pro.local>
2021-10-14 15:22:53 +0700

    add

--- backend/app/app/api/api_v1/endpoints/book.py
+++ backend/app/app/api/api_v1/endpoints/book.py
@@ -14,7 +14,7 @@ router = APIRouter()
 def read_books(
     db: Session = Depends(deps.get_db),
     skip: int = 0,
-    limit: int = 100,
+    limit: int = 10000,
     current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
@@ -23,8 +23,8 @@ def read_books(
     # if crud.user.is_superuser(current_user):
     #     books = crud.book.get_multi(db, skip=skip, limit=limit)
     # else:
-    books = crud.book.get_multi(
-        db=db, skip=skip
+    books = crud.book.get_multi_by_owner(
+        db=db, owner_id=current_user.id, skip=skip, limit=limit
     )
     return books
 
--- backend/app/app/api/api_v1/endpoints/customer.py
+++ backend/app/app/api/api_v1/endpoints/customer.py
@@ -14,17 +14,17 @@ router = APIRouter()
 def read_customers(
     db: Session = Depends(deps.get_db),
     skip: int = 0,
-    limit: int = 100,
+    limit: int = 10000,
     current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
     Retrieve customers.
     """
     if crud.user.is_superuser(current_user):
-        customers = crud.customer.get_multi(db, skip=skip)
+        customers = crud.customer.get_multi(db, skip=skip, limit=limit)
     else:
-        customers = crud.customer.get_multi(
-            db=db, skip=skip
+        customers = crud.customer.get_multi_by_owner(
+            db=db, owner_id=current_user.id, skip=skip, limit=limit
         )
     return customers
 
